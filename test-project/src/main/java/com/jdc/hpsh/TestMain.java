78900e639ba1ad73f62b107dc2bb3eca68083a23
Hnin Phyu Sin Htut <hninphyusinhtut1484@gmail.com>
2025-09-30 14:11:48 +0630

    Update TestMain.java

--- test-project/src/main/java/com/jdc/hpsh/TestMain.java
+++ test-project/src/main/java/com/jdc/hpsh/TestMain.java
@@ -3,6 +3,7 @@ package com.jdc.hpsh;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("First Test of Git!");
+		System.out.println("First Test of Git!!!");
 	}
 }
+
